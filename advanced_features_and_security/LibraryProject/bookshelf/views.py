f980e4586208fa48ce9e61cdc7dc326b922d3b81
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 20:06:23 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from .models import Book
+
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'templates/relationship_app/book_list.html', {'books': books})
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    # Logic to edit a book
+    pass
 
 # Create your views here.
