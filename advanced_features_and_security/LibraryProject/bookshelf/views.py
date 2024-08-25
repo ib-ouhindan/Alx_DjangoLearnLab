49a472461fb1595b3a9e0f1e42549c95af39253b
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 20:14:34 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
-
+from .forms import ExampleForm
 @permission_required('relationship_app.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
