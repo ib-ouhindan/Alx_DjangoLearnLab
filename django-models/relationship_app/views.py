03747daa7490a3e69ef043871cb5d2121f8aa403
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 16:56:10 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 def list_books(request):
     books = Book.objects.all()
@@ -15,3 +21,22 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
+# Login view
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration view
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('login')
