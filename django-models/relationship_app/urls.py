c18327693911a5e0bed0945fb3634c54d9d5a321
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 16:36:29 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import list_books,LibraryDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),
