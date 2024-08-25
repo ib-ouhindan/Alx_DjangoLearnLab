01242f72938ba295d332d7e7f2f95f5529fcb774
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 20:05:33 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -38,3 +38,14 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
