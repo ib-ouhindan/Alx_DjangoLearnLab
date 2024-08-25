ce5982befbdb9ef351f848f4f9d6586db77208c9
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 18:05:36 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,24 +11,24 @@ from django.shortcuts import redirect
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'templates/relationship_app/list_books.html', {'books': books})
 
 from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'templates/relationship_app/library_detail.html'
     context_object_name = 'library'
 
 
 # Login view
 class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
+    template_name = 'templates/relationship_app/login.html'
 
 # Logout view
 class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
+    template_name = 'templates/relationship_app/logout.html'
 
 # Registration view
 class RegisterView(CreateView):
