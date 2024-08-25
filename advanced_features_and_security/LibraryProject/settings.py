2ca519beec8e40e8a17d87a44bb2f010874ad6a0
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 20:20:30 +0100

    Update settings.py

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -121,3 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # One year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
