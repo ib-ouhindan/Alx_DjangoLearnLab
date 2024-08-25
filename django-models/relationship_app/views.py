fb2b25a8ed369c74aabd0d031245e6454e68b232
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 18:35:33 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -54,12 +54,12 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'templates/admin_view.html')
+    return render(request, 'templates/relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'templates/librarian_view.html')
+    return render(request, 'templates/relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'templates/member_view.html')
+    return render(request, 'templates/relationship_app/member_view.html')
