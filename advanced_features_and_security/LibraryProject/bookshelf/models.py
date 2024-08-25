ecd7d8ab617d27dbea5f4ceb49b97c2bfca52de3
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-08-25 19:21:50 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -25,6 +25,11 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+        def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
