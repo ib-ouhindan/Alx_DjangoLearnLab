0b57a439b37f7e82626c48d83bf93580c8cbda81
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-09-01 19:48:32 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,6 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()  # Retrieves all Book objects
     serializer_class = BookSerializer  # Uses the BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
