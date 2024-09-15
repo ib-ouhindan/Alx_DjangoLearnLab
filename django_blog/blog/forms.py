bc407664141e22a279e4b288adf7dc2ca2da4b6b
Ouhindan <101867910+ib-ouhindan@users.noreply.github.com>
2024-09-15 19:47:43 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,7 +6,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
+     widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter your title'}),
+            'content': forms.Textarea(attrs={'placeholder': 'Write your content here...'}),
+        }
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
